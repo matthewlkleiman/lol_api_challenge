2a64876754b424b4909fd82c60f675881154e03b
Matt Kleiman <mkleiman@avvo.com>
2015-08-19 09:42:27 -0700

    * Wrote driver * Now method checks status code * Added in more error checks * Driver also handles service rate limit as well

--- driver.py
+++ driver.py
@@ -3,22 +3,77 @@ __author__ = 'mkleiman'
 File written to scrape data from riot's api
 """
 
-import time
-from math import floor
+import csv
+import json
+import sys
+from time import sleep, strftime
+from os import listdir
+from os.path import isfile, join
+from re import search
+from lol_api_request import make_api_call
 
 # Config
-api_keys = ['4fc5d1d3-817b-4a0e-b86d-ad6436bb9391']
+RATE_LIMIT = 5
 
 resource_folders = ['AP_ITEM_DATASET/5.11/NORMAL_5x5', 'AP_ITEM_DATASET/5.11/RANKED_SOLO',
                     'AP_ITEM_DATASET/5.14/NORMAL_5x5', 'AP_ITEM_DATASET/5.14/RANKED_SOLO']
 
-output_folder = 'output'
-output_name = 'game_info.csv'
+output_file = 'output/game_info.csv'
+headers = ['patch_num', 'game_mode', 'match_id', 'match_duration', 'champ', 'team', 'creep_per_min_z_to_10',
+           'creep_per_min_10_to_20', 'creep_per_min_20_to_30', 'creep_per_min_30_to_e', 'role', 'lane', 'did_win',
+           'item0', 'item1', 'item2', 'item3', 'item4', 'item5', 'kills', 'deaths', 'assists', 'doubleKills',
+           'tripleKills', 'quadraKills', 'pentaKills', 'unrealKills', 'total_heal', 'gold_earned', 'gold_spent',
+           'crowd_control_dealt_time']
 
-RATE_LIMIT = floor(len(api_keys) * 500 / 600.0)
+input_files = []
 
-print RATE_LIMIT
+# Get all json files to iterate through
+for folder in resource_folders:
+    files_in_folder = [f for f in listdir(folder) if isfile(join(folder, f))]
+    for json_file in files_in_folder:
+        input_files.append(folder + '/' + json_file)
 
-request_counter = 0
-if request_counter > RATE_LIMIT:
-    time.sleep(1)
+# Create output
+with open(output_file, 'w') as f:
+    csv_writer = csv.DictWriter(f, fieldnames=headers, lineterminator='\n')
+    csv_writer.writeheader()
+    for input_file in input_files:
+        print '[' + strftime('%H:%M:%S') + '] Reading Input file: ' + input_file
+        # Read input
+        with open(input_file, 'r') as g:
+            server = search('\/(\w{2})\.', g.name).group(1).lower()
+            patch_num = search('5.{3}', g.name).group(0)
+            game_mode = search('\/([A-Z]+\w*)', g.name).group(1)
+
+            decoded_json = json.load(g)
+            request_count = 0
+
+            # make api calls
+            total_calls_made = 0
+            print '[' + strftime('%H:%M:%S') + '] Began sending API requests'
+            for game_id in decoded_json:
+                game_url = 'https://' + server + '.api.pvp.net/api/lol/' + server + '/v2.2/match/' + str(game_id)
+                sleep(1)
+                game = make_api_call(game_url, {'api_key': 'INSERT API KEY'})
+                while game == 429:
+                    print '[' + strftime('%H:%M:%S') + '] Error, api call failed. Total calls made: ' + str(
+                        total_calls_made)
+                    status_code, game = make_api_call(game_url, {'api_key': 'INSERT API KEY'})
+                    exit()
+
+                request_count += 1
+                total_calls_made += 1
+
+                if request_count == RATE_LIMIT:
+                    sys.stdout.write(
+                        '[' + strftime('%H:%M:%S') + '] Rate limit reached, now sleeping for 15 seconds...')
+                    sleep(15)
+                    request_count = 0
+                    print 'Resuming...(calls made: ' + str(total_calls_made) + ')'
+
+                # write to output file
+                for player_row in game:
+                    player_row.update({'patch_num': patch_num, 'game_mode': game_mode})
+                    if not set(player_row.keys()).issubset(headers):
+                        raise IndexError('Error: some values in row are\'nt in output headers')
+                csv_writer.writerows(game)
--- lol_api_request.py
+++ lol_api_request.py
@@ -2,70 +2,62 @@
 Riot API scraper
 """
 import requests
-import csv
-
-# Currently all data is in here, but this isn't meant to be run as a __main__
-api_key = {'api_key': 'INSERT_API_KEY'}
-headers = ['champ', 'team', 'cs_diff_per_min_z_to_10', 'cs_diff_per_min_10_to_20', 'cs_diff_per_min_20_to_30',
-           'cs_diff_per_min_30_to_e', 'role', 'lane', 'did_win', 'item0', 'item1', 'item2', 'item3', 'item4', 'item5',
-           'kills', 'deaths', 'assists', 'doubleKills', 'tripleKills', 'quadraKills', 'pentaKills', 'unrealKills',
-           'total_heal', 'gold_earned', 'gold_spent', 'crowd_control_dealt_time']
 
 
 def make_api_call(url, params_dict):
     rows = []
-    r = requests.get(url, params=params_dict)
-    if r.status_code == 200:
-        game_json = r.json()
-        for summoner in game_json['participants']:
-            row = {'champ': str(summoner['championId']), 'team': str(summoner['teamId'])}
-            try:
-                cs_diff_per_min_keys = ['cs_diff_per_min_z_to_10', 'cs_diff_per_min_10_to_20',
-                                        'cs_diff_per_min_20_to_30', 'cs_diff_per_min_30_to_e']
-                cs_diff_per_min_values = summoner['timeline']['csDiffPerMinDeltas'].values()
-                cs_diff_per_min_deltas = dict(zip(cs_diff_per_min_keys, cs_diff_per_min_values))
-            except ValueError:
-                print 'Failed to read stats'
-
-            try:
-                row['role'] = str(summoner['timeline']['role'])
-                row['lane'] = str(summoner['timeline']['lane'])
-            except ValueError:
-                print 'Timeline not available'
-
-            stats = summoner['stats']
-            row['did_win'] = bool(stats['winner'])
-            for i in xrange(6):
+    try:
+        r = requests.get(url, params=params_dict)
+        if r.status_code == 200:
+            game_json = r.json()
+            for summoner in game_json['participants']:
+                row = {'champ': str(summoner['championId']), 'team': str(summoner['teamId']),
+                       'match_id': str(game_json['matchId']), 'match_duration': str(game_json['matchDuration'])}
                 try:
-                    item_num = 'item' + str(i)
-                    row[item_num] = stats[item_num]
-                except IndexError:
-                    print 'Item(s) not available'
+                    creep_per_min_keys = ['creep_per_min_z_to_10', 'creep_per_min_10_to_20', 'creep_per_min_20_to_30',
+                                          'creep_per_min_30_to_e']
+                    creep_per_min_values = summoner['timeline']['creepsPerMinDeltas'].values()
+                    creep_per_min_deltas = dict(zip(creep_per_min_keys, creep_per_min_values))
+                except ValueError:
+                    print 'Failed to read stats'
+                except KeyError:
+                    print 'Failed reading summoner ' + str(summoner['participantId']) + str(game_json['matchId'])
+                    print creep_per_min_deltas
 
-            multi_kills = {'doubleKills': stats['doubleKills'], 'tripleKills': stats['tripleKills'],
-                          'quadraKills': stats['quadraKills'], 'pentaKills': stats['pentaKills'],
-                          'unrealKills': stats['unrealKills']}
-            kda = {'kills': stats['kills'], 'deaths': stats['deaths'], 'assists': stats['assists']}
-            row['total_heal'] = stats['totalHeal']
-            gold = {'gold_earned': stats['goldEarned'], 'gold_spent': stats['goldSpent']}
-            row['crowd_control_dealt_time'] = stats['totalTimeCrowdControlDealt']
-            row.update(multi_kills)
-            row.update(kda)
-            row.update(gold)
-            row.update(cs_diff_per_min_deltas)
-            rows.append(row)
-        return rows
+                try:
+                    row['role'] = str(summoner['timeline']['role'])
+                    row['lane'] = str(summoner['timeline']['lane'])
+                except ValueError:
+                    print 'Timeline not available'
 
-# Run method
-game_url = 'https://na.api.pvp.net/api/lol/na/v2.2/match/1852548676'
-game = make_api_call(game_url, api_key)
+                stats = summoner['stats']
+                row['did_win'] = bool(stats['winner'])
+                for i in xrange(6):
+                    try:
+                        item_num = 'item' + str(i)
+                        row[item_num] = stats[item_num]
+                    except IndexError:
+                        print 'Item(s) not available'
 
-# Write output
-with open('output/test.csv', 'w') as f:
-    csv_writer = csv.DictWriter(f, fieldnames=headers, lineterminator='\n')
-    csv_writer.writeheader()
-    for player_row in game:
-        if not set(player_row.keys()).issubset(headers):
-            raise IndexError('Error: some values in row are\'nt in output headers')
-
-    csv_writer.writerows(game)
+                multi_kills = {'doubleKills': stats['doubleKills'], 'tripleKills': stats['tripleKills'],
+                               'quadraKills': stats['quadraKills'], 'pentaKills': stats['pentaKills'],
+                               'unrealKills': stats['unrealKills']}
+                kda = {'kills': stats['kills'], 'deaths': stats['deaths'], 'assists': stats['assists']}
+                row['total_heal'] = stats['totalHeal']
+                gold = {'gold_earned': stats['goldEarned'], 'gold_spent': stats['goldSpent']}
+                row['crowd_control_dealt_time'] = stats['totalTimeCrowdControlDealt']
+                row.update(multi_kills)
+                row.update(kda)
+                row.update(gold)
+                row.update(creep_per_min_deltas)
+                rows.append(row)
+                r.close()
+        else:
+            print r.headers
+            r.close()
+            print 'Bad request code: ' + str(r.status_code)
+            print url
+            return r.status_code
+    except requests.exceptions.ConnectionError:
+        print 'Connection to ' + url + ' failed'
+    return rows
